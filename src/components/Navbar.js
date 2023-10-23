cf8aeb7ceffa11050aa82b66abc72e3408e65f1b
Amal Krishna <amalkrishna528@gmail.com>
2023-10-23 15:49:26 +0530

    Navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="netflix">
+            </div>
+            <div className="list">
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movie</span>
+                <span>Popular</span>
+            </div>
+        </div>
+    )
+}
